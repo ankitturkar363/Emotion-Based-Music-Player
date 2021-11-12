8c3b231595d5b2a7177dc0ed8c3656ed0470206b
ankitturkar363 <ankitturkar363@gmail.com>
2021-11-12 20:46:53 +0530

    my app first commit

---
+++ main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from main import views
+
+urlpatterns = [
+    
+
+    path('', views.index, name="index"),
+    path("expression", views.expression, name="expression")
+]
